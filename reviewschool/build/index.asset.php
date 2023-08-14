4023d3a008ca8db1dfa64ca1dc57a6ea3b66b341
Bogning Rodney <bogningrodney2@gmail.com>
2023-08-14 10:24:52 +0200

    mise a jour

--- reviewschool/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '0796c37e861ca952b049');
--- reviewschool/templates/app.php
+++
@@ -1,3 +0,0 @@
-<div id="rankmathtest">
-    <h2>Loading...</h2>
-</div>
