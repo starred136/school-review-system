42c2ae42a56648ff385b0dddb11e1d5649d6625f
Bogning Rodney <bogningrodney2@gmail.com>
2023-08-17 13:14:04 +0200

    slug

--- Pages/all-categories/index.js
+++ Pages/all-categories/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 
-export default function Categories(){
+export const getStaticProps = async () => {
+  const posts = await fetch("http://localhost/wordpress/wp-json/wp/v2/posts").then(
+    (res) => res.json()
+  );
+
+  return { props: { posts } };
+};
+
+export default function Categories( props ){
     return(
         <>
         <main>
@@ -52,133 +60,32 @@ export default function Categories(){
       <p>Cum doctus civibus efficiantur in imperdiet deterruisset.</p>
     </div>
     <div className="row justify-content-center">
+
+
+      {props.posts.map(item => (
       <div className="col-lg-3 col-6">
-        <a href="grid-listings-filterstop.html" className="box_cat_home">
-          <img src="assets/img/icon_home_1.svg" width={65} height={65} alt="" />
-          <h3>Clothing</h3>
-          <ul className="clearfix">
-            <li>
-              <strong>1,023</strong> Results
-            </li>
-            <li>
-              <strong>2,435</strong>
-              <i className="icon-comment" />
-            </li>
-          </ul>
-        </a>
-      </div>
-      <div className="col-lg-3 col-6">
-        <a href="grid-listings-filterstop.html" className="box_cat_home">
-          <img src="assets/img/icon_home_2.svg" width={65} height={65} alt="" />
-          <h3>Hotels</h3>
-          <ul className="clearfix">
-            <li>
-              <strong>856</strong> Results
-            </li>
-            <li>
-              <strong>455</strong>
-              <i className="icon-comment" />
-            </li>
-          </ul>
-        </a>
-      </div>
-      <div className="col-lg-3 col-6">
-        <a href="rgrid-listings-filterstop.html" className="box_cat_home">
-          <img src="assets/img/icon_home_3.svg" width={65} height={65} alt="" />
-          <h3>Restaurants</h3>
-          <ul className="clearfix">
-            <li>
-              <strong>2,400</strong> Results
-            </li>
-            <li>
-              <strong>1,323</strong>
-              <i className="icon-comment" />
-            </li>
-          </ul>
-        </a>
-      </div>
-      <div className="col-lg-3 col-6">
-        <a href="grid-listings-filterstop.html" className="box_cat_home">
-          <img src="assets/img/icon_home_4.svg" width={65} height={65} alt="" />
-          <h3>Bars</h3>
-          <ul className="clearfix">
-            <li>
-              <strong>854</strong> Results
-            </li>
-            <li>
-              <strong>345</strong>
-              <i className="icon-comment" />
-            </li>
-          </ul>
-        </a>
-      </div>
-      <div className="col-lg-3 col-6">
-        <a href="grid-listings-filterstop.html" className="box_cat_home">
-          <img src="assets/img/icon_home_8.svg" width={65} height={65} alt="" />
-          <h3>Electronics</h3>
-          <ul className="clearfix">
-            <li>
-              <strong>1,210</strong> Results
-            </li>
-            <li>
-              <strong>530</strong>
-              <i className="icon-comment" />
-            </li>
-          </ul>
-        </a>
-      </div>
-      <div className="col-lg-3 col-6">
-        <a href="grid-listings-filterstop.html" className="box_cat_home">
-          <img src="assets/img/icon_home_5.svg" width={65} height={65} alt="" />
-          <h3>Beauty</h3>
-          <ul className="clearfix">
-            <li>
-              <strong>1,343</strong> Results
-            </li>
-            <li>
-              <strong>315</strong>
-              <i className="icon-comment" />
-            </li>
-          </ul>
-        </a>
-      </div>
-      <div className="col-lg-3 col-6">
-        <a href="grid-listings-filterstop.html" className="box_cat_home">
-          <img src="assets/img/icon_home_6.svg" width={65} height={65} alt="" />
-          <h3>Fitness</h3>
-          <ul className="clearfix">
-            <li>
-              <strong>678</strong> Results
-            </li>
-            <li>
-              <strong>123</strong>
-              <i className="icon-comment" />
-            </li>
-          </ul>
-        </a>
-      </div>
-      <div className="col-lg-3 col-6">
-        <a href="grid-listings-filterstop.html" className="box_cat_home">
-          <img src="assets/img/icon_home_7.svg" width={65} height={65} alt="" />
-          <h3>Doctors</h3>
-          <ul className="clearfix">
-            <li>
-              <strong>378</strong> Results
-            </li>
-            <li>
-              <strong>560</strong>
-              <i className="icon-comment" />
-            </li>
-          </ul>
-        </a>
-      </div>
+      <a href="grid-listings-filterstop.html" className="box_cat_home">
+        <img src="assets/img/icon_home_1.svg" width={65} height={65} alt="" />
+        <h3>Clothing</h3>
+        <ul className="clearfix">
+          <li>
+            <strong>1,023</strong> Results
+          </li>
+          <li>
+            <strong>2,435</strong>
+            <i className="icon-comment" />
+          </li>
+        </ul>
+      </a>
+    </div>
+      ))}
     </div>
   </div>
   {/* /container */}
   <div className="bg_color_1">
     <div className="container margin_60_35">
       <div className="main_title_3 text-center">
-        <h2>Vanno companies categories</h2>
+        <h2>Schools categories</h2>
         <p>Cum doctus civibus efficiantur in imperdiet deterruisset.</p>
       </div>
       <div className="row justify-content-center">
@@ -190,141 +97,13 @@ export default function Categories(){
                   <strong>543</strong>Cars, motorbikes &amp; accessories
                 </a>
               </li>
+              { props.posts.map(item => (
               <li>
-                <a href="reviews-page.html">
-                  <strong>32</strong>Clothing
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>213</strong>Floristry
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>54</strong>Books
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>225</strong>Office &amp; business
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>432</strong>Computers &amp; electronics
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>244</strong>Chemists &amp; cosmetics
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>243</strong>Precious metals
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>78</strong>Energy
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>898</strong>Finance &amp; insurance
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>657</strong>Photo, print &amp; book-on-demand
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>53</strong>Gardening supplies
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>654</strong>Gifts
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>90</strong>Household goods
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>89</strong>Luggage, bags &amp; leather goods
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>789</strong>Food
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>45</strong>Pharmaceuticals
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>122</strong>Furniture &amp; decoration
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>796</strong>Telecommunication
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>56</strong>Music &amp; film
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>423</strong>Opticians
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>331</strong>Travel &amp; hotels
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>32</strong>Tickets
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>432</strong>Jewellery &amp; watches
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>21</strong>Shoes
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>90</strong>Sports
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>85</strong>Pet supplies
-                </a>
-              </li>
-              <li>
-                <a href="reviews-page.html">
-                  <strong>65</strong>Consulting
-                </a>
-              </li>
+              <a href="reviews-page.html">
+                <strong>543</strong>Cars, motorbikes &amp; accessories
+              </a>
+            </li>
+              ))}
             </ul>
           </div>
         </div>
